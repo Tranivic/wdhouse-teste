2c226f70cc80901c17ec9cc4201f21bf0b41990f
Tranivic <victor.trani1@gmail.com>
2023-08-22 22:23:07 -0300

    feat: slug(catchall) page create

---
+++ pages/[...slug].vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container-fluid h-100vh text-center pt-5">
+    <p>
+      Página não encontrada. Redirecionando para a página inicial em
+      {{ counter }} segundos...
+    </p>
+  </div>
+</template>
+
+<script setup>
+const counter = ref(5);
+
+const redirectToHome = async () => {
+  const route = useRouter();
+  const intervalId = setInterval(() => {
+    counter.value--;
+    if (counter.value === 0) {
+      clearInterval(intervalId);
+      console.log("Redirecionando...");
+      route.push("/");
+    }
+  }, 1000);
+};
+
+onMounted(() => {
+  redirectToHome();
+});
+</script>
